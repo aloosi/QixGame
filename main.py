90d25ee9137ea4ae30fc927027e840db67f01ac4
A Alhadi Al-Aloosi <ahmad@aloosi.ca>
2021-03-05 04:36:11 -0500

    main menu display functionality implemented

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ main_background = pygame.image.load('img/main_background.png')
 main_background = pygame.transform.scale(main_background, (800,800))
 
 main_menu = pygame.image.load('img/main_menu.png')
-main_menu = pygame.transform.scale(main_background, (800,800))
+main_menu = pygame.transform.scale(main_menu, (800,800))
 
 game_state = 0 # Main Screen
 
@@ -35,7 +35,7 @@ while running:
             running = False
         
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_RETURN and game_state == 0:
                 screen.blit(main_menu, (0,0))
                 pygame.display.update()
                 game_state = 1

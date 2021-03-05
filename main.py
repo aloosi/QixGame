a1b7325fad68a755cfa62bc1e25ecf4e81323d9a
A Alhadi Al-Aloosi <ahmad@aloosi.ca>
2021-03-05 04:26:46 -0500

    Implementing main menu screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+from pygame_functions import *
 
 
 # Initializes pygame
@@ -17,16 +18,27 @@ pygame.display.set_caption("Qix")
 main_background = pygame.image.load('img/main_background.png')
 main_background = pygame.transform.scale(main_background, (800,800))
 
+main_menu = pygame.image.load('img/main_menu.png')
+main_menu = pygame.transform.scale(main_background, (800,800))
 
 game_state = 0 # Main Screen
 
+# Main background initiation
+screen.blit(main_background, (0,0))
+pygame.display.update()
+
 running = True
 while running:
-    if game_state == 0:
-        screen.blit(main_background, (0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                screen.blit(main_menu, (0,0))
+                pygame.display.update()
+                game_state = 1
 
     pygame.display.update()
 

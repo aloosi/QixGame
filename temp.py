14de61918ebd716737ce5a11491ad0a2df025efe
SEricL <73135596+SEricL@users.noreply.github.com>
2021-03-24 13:06:03 -0400

    Updated Code.
    Collision implemented, requires checker/stopper to prevent instant death.

--- temp.py
+++ temp.py
@@ -1,6 +1,13 @@
 import pygame
 import sys
 from pygame.locals import *
+import time
+
+TOPLEFT = (50,50)
+TOPRIGHT = (450,50)
+BOTTOMLEFT = (50,450)
+BOTTOMRIGHT = (450,450)
+PLAYER_LIVES = 3
 
 pygame.init()
 
@@ -23,13 +30,12 @@ class Sparx(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("spark.png")
-        self.surf = pygame.Surface((5,5))
+        self.surf = pygame.Surface((9,9))
         self.rect = self.surf.get_rect(center=(50,50))
 
     def move(self):
         x = self.rect.centerx
         y = self.rect.centery
-        print(x,y)
         if y == 50:
             if x < 450:
                 self.rect.move_ip(5,0)
@@ -51,7 +57,6 @@ class Sparx(pygame.sprite.Sprite):
             else:
                 self.rect.move_ip(5,0)
 
-
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
@@ -60,11 +65,11 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("player.png")
         self.surf = pygame.Surface((10,10))
-        self.rect = self.surf.get_rect()
+        self.rect = self.surf.get_rect(center=(BOTTOMRIGHT))
 
     def update(self):
         key = pygame.key.get_pressed()
-        if self.rect.centerx < 400:
+        if self.rect.centerx < 450:
             if key[K_RIGHT]:
                 self.rect.move_ip(5,0)
         if self.rect.centerx > 50:
@@ -83,6 +88,19 @@ class Player(pygame.sprite.Sprite):
 
 P1 = Player()
 E1 = Sparx()
+enemies = pygame.sprite.Group()
+enemies.add(E1)
+sprites = pygame.sprite.Group()
+sprites.add(P1)
+sprites.add(E1)
+font = pygame.font.SysFont("Times New Roman",16)
+temp = "Lives:",str(PLAYER_LIVES)
+lives = font.render(("Lives: "+str(PLAYER_LIVES)),True,RED)
+livesRect = lives.get_rect()
+livesRect.topright = (500,0)
+lose = font.render('You lost!',True,RED,BLACK)
+loseRect = lose.get_rect()
+loseRect.center = (250,250)
 
 while True:
     pygame.display.update()
@@ -92,11 +110,21 @@ while True:
     pygame.draw.rect(display, WHITE, (50, 50, 400, 400), 1)
     P1.draw(display)
     E1.draw(display)
+    lives = font.render(("Lives: "+str(PLAYER_LIVES)), True, RED)
+    display.blit(lives,livesRect)
     pygame.display.update()
     FPS.tick(50)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-
-
+    if pygame.sprite.spritecollideany(P1, enemies):
+        if PLAYER_LIVES <= 1:
+            display.blit(lose,loseRect)
+            pygame.display.update()
+            time.sleep(4)
+            pygame.quit()
+            sys.exit()
+        else:
+            PLAYER_LIVES -= 1
+            # TODO:Move sparx, keep player in initial spot.
